d41639d38df1e89f12954b312daf3b4fb60ec4de
Ward Peeters <ward@coding-tech.com>
2020-11-22 14:56:26 +0100

    add remove helpers

--- index.js
+++ index.js
@@ -1,12 +1,83 @@
+import { RemoveHandler } from './lib/removeHandler'
+import { RemoveLink } from './lib/removeLink'
+
+const DOMAIN_HOST = 'www.gatsbyjs.com'
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event))
 })
+
+async function handleDocument(res, searchParams) {
+  const rewriter = new HTMLRewriter()
+
+  if (searchParams.has('remove-inline-scripts')) {
+    rewriter.on(
+      'script:not([src])',
+      new RemoveHandler(searchParams.get('remove-inline-scripts').split(',')),
+    )
+  }
+
+  if (searchParams.has('remove-external-scripts')) {
+    rewriter.on('script[src]', new RemoveHandler())
+  }
+
+  if (searchParams.has('remove-preload')) {
+    rewriter.on(
+      'link',
+      new RemoveLink({
+        rel: 'preload',
+      }),
+    )
+  }
+
+  if (searchParams.has('remove-selectors')) {
+    rewriter.on(
+      searchParams.get('remove-selectors'),
+      new RemoveLink(RemoveHandler),
+    )
+  }
+
+  const transformedResponse = await rewriter.transform(res)
+  const newResponse = new Response(transformedResponse.body, res)
+
+  if (newResponse) {
+    newResponse.headers.delete('link')
+  }
+
+  return newResponse
+}
+
 /**
  * Respond with hello worker text
- * @param {Request} request
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+async function handleRequest(event) {
+  /**
+   * @type {Request} request
+   */
+  const request = event.request
+  const parsedUrl = new URL(request.url)
+
+  parsedUrl.host = DOMAIN_HOST
+
+  const fetchUrl = `${parsedUrl.origin}${parsedUrl.pathname}`
+
+  const cache = caches.default
+  const cacheKey = parsedUrl.toString()
+
+  let res = await cache.match(cacheKey)
+
+  if (!res) {
+    res = await fetch(fetchUrl)
+
+    // handle navigate
+    if (res.headers.get('Content-Type').includes('text/html')) {
+      res = await handleDocument(res, parsedUrl.searchParams)
+
+      res.headers.set('Cache-Control', 'max-age=300')
+    }
+
+    event.waitUntil(cache.put(cacheKey, res.clone()))
+  }
+
+  return res
 }
